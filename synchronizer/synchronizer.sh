0cd3c3a288da558fd26b057fa407609ecf02e6f6
Nyasara <nyasara2@gmail.com>
2014-11-27 17:28:02 -0500

    Upgrades to synchronizer script

--- synchronizer/synchronizer.sh
+++ synchronizer/synchronizer.sh
@@ -6,23 +6,38 @@ export ETCDCTL_PEERS
 
 # Make sure the core package is up to date and set the ready key
 
-for container in `etcdctl ls /synchronizer/packages/core/containers`
+etcdpackagedir="/synchronizer/packages/core/containers"
+for container in `etcdctl ls $etcdpackagedir`
 do
-    git clone `etcdctl get /synchronizer/packages/core/containers/$container/repo`
-    cp -r `etcdctl get /synchronizer/packages/core/containers/$container/repopath` /srv/core/containers/$container
-    version=`cat /srv/core/containers/$container/container_version`
-    etcdctl set /synchronizer/packages/core/containers/$container/container_version $container_version
-    version=`cat /srv/core/containers/$container/config_version`
-    etcdctl set /synchronizer/packages/core/containers/$container/config_version $config_version
+    method=`etcdctl get $etcdpackagedir/$container/method`
+    if [ "$method" = "git" ]
+    then
+        etcdcontainerdir="$etcdpackagedir/$container"
+        echo "Get container $container via git in $etcdcontainerdir"
+        git clone `etcdctl get $etcdcontainerdir/repo` core-$container
+        echo "git clone `etcdctl get $etcdcontainerdir/repo` core-$container"
+        cp -r core-$container/`etcdctl get $etcdcontainerdir/repopath` /srv/core/containers/$container
+        echo "cp -r core-$container/`etcdctl get $etcdcontainerdir/repopath` /srv/core/containers/$container"
+        version=`cat /srv/core/containers/$container/container_version`
+        echo "version=`cat /srv/core/containers/$container/container_version`"
+        echo "etcdctl set $etcdcontainerdir/container_version $container_version"
+        etcdctl set $etcdcontainerdir/container_version $container_version
+        version=`cat /srv/core/containers/$container/config_version`
+        echo "version=`cat /srv/core/containers/$container/config_version`"
+        etcdctl set $etcdcontainerdir/config_version $config_version
+        echo "etcdctl set $etcdcontainerdir/config_version $config_version"
+    elif [ "$method" = "azureblob" ]
+
+    fi
 done
 etcdctl set /synchronizer/ready 1
-
+ 
 # Loop forever
 while [ 1 ]
 do
     # Check the system for the kill signal indicating we should shut down
     killsignal=`etcdctl get /cluster/runstate`
-    if [ $killsignal = "1" ]
+    if [ "$killsignal" = "1" ]
     then
         break
     fi
@@ -45,4 +60,6 @@ do
             etcdctl set /synchronizer/packages/core/containers/$container/config_version $config_version
         done
     done
+    echo "Synchronizer check cycle complete"
+    sleep 60
 done
