99afd061cf2392b582905fd417a76174c393a62c
Nyasara <nyasara2@gmail.com>
2014-11-28 13:57:18 -0500

    Updates etcdwatcher

--- etcdwatcher/etcd-watch.sh
+++ etcdwatcher/etcd-watch.sh
@@ -5,22 +5,28 @@ export ETCDCTL_PEERS
 
 while [ 1 ] 
 do
-    # Read in all the values from the file and update them in etcd
-    for keyfile in `ls /etc/corestrap/keys.d`
+    for package in `ls /srv`
     do
-        while read key value
+        for container in `ls /srv/$package/containers`
         do
-            OLDVAL=`etcdctl get $key`
-            if [ "$OLDVAL" != "$value" ]
-            then
-                etcdctl set $key "$value"
-                echo "Updated"
-            fi
-            echo $key
-            echo $value
-        done < /etc/corestrap/keys.d/$keyfile
-        etcdctl set /corestrap/status 1
-        sleep 60
+            # Read in all the values from the file and update them in etcd
+            for keyfile in `ls /srv/$package/containers/$container/keys`
+            do
+                while read key value
+                do
+                    OLDVAL=`etcdctl get $key`
+                    if [ "$OLDVAL" != "$value" ]
+                    then
+                        etcdctl set $key "$value"
+                        echo "Updated"
+                    fi
+                    echo $key
+                    echo $value
+                done < /srv/$package/containers/$container/keys/$keyfile
+            done
+        done
     done
+    etcdctl set /etcdwatcher/status 1                
+    sleep 60
 done
-etcdctl rm /corestrap/status
+etcdctl rm /etcdwatcher/status
